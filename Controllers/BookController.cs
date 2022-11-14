1e3e97c5f0f0d4c1b0a69541898cefc84b0f40ba
ViktoD <90144863+ViktoD@users.noreply.github.com>
2022-11-14 09:43:11 +0200

    Update BookController.cs

--- Controllers/BookController.cs
+++ Controllers/BookController.cs
@@ -1,4 +1,4 @@
-﻿using lab7.Server.Database;
+using lab7.Server.Database;
 using lab7.Shared.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -18,44 +18,36 @@ namespace lab7.Server.Controllers
         }
 
         [HttpGet]
-        [Route("GetBooks")]
         public async Task<ActionResult<List<Book>>> GetBooks()
         {
             return await _db.Books.OrderBy(p => p.Name).ToListAsync();
         }
 
         [HttpGet("{id:int}")]
-        [Route("GetBook/{id:int}")]
         public async Task<ActionResult<Book>> GetBook(int id)
         {
             return await _db.Books.FirstAsync(p => p.ID == id);
         }
 
         [HttpPost]
-        [Route("AddBook")]
-        public async Task<ActionResult<Book>> AddBook(Book book)
+        public async Task AddBook(Book book)
         {
             await _db.Books.AddAsync(book);
             await _db.SaveChangesAsync();
-            return book;
         }
 
-        [HttpPost]
-        [Route("EditBook")]
-        public async Task<ActionResult<Book>> EditBook(Book book)
+        [HttpPut]
+        public async Task EditBook(Book book)
         {
             _db.Books.Update(book);
             await _db.SaveChangesAsync();
-            return book;
         }
 
-        [HttpPost]
-        [Route("DeleteBook")]
-        public async Task<ActionResult<Book>> DeleteBook(Book book)
+        [HttpDelete("{id:int}")]
+        public async Task DeleteBook(int id)
         {
-            _db.Books.Remove(book);
+            _db.Books.Remove(await _db.Books.FirstAsync(p => p.ID == id));
             await _db.SaveChangesAsync();
-            return book;
         }
     }
 }
